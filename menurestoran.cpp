48abf739995353c9e03b9eb543792454e3e00b26
Nadindra MA <107752721+NMATech@users.noreply.github.com>
2022-10-29 12:24:41 +0700

    Update menurestoran.cpp

--- menurestoran.cpp
+++ menurestoran.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main () {
     char pilihan;
 
-    cout << "Selamat datang di restoran MC'Yahud !" << endl;
-    cout << "\nBerikut adalah menu yang ada :" << endl;
-    cout << "1. Nasi Goreng Informatika Rp. 5.000" << endl;
-    cout << "2. Nasi Soto Ayam Internet Rp. 7.000" << endl;
-    cout << "3. Gado-gado Diskrit Rp. 4.500" << endl;
-    cout << "4. Bubur Ayam LAN Rp. 4.000" << endl;
+    cout << "\n           Menu restoran MC'Yahud !        " << endl;
+    cout << "=============================================" << endl;
+    cout << "1. Nasi Goreng Informatika     Rp. 5.000,-" << endl;
+    cout << "2. Nasi Soto Ayam Internet     Rp. 7.000,-" << endl;
+    cout << "3. Gado-gado Diskrit           Rp. 4.500,-" << endl;
+    cout << "4. Bubur Ayam LAN              Rp. 4.000,-" << endl;
+    cout << "=============================================" << endl;
 
     cout << "\nMasukkan pilihan anda : " << endl;
     cin >> pilihan;
@@ -17,19 +18,19 @@ int main () {
     switch (pilihan)
     {
         case '1':
-            cout << "\npilihan No 1. Nasi Goreng Informatika Rp. 5.000";
+            cout << "pilihan No 1. Nasi Goreng Informatika Rp. 5.000";
             break;
         case '2':
-            cout << "\npilihan No 2. Nasi Soto Ayam Internet Rp. 7.000";
+            cout << "pilihan No 2. Nasi Soto Ayam Internet Rp. 7.000";
             break;
         case '3':
-            cout << "\npilihan No 3. Gado-gado Diskrit Rp. 4.500";
+            cout << "pilihan No 3. Gado-gado Diskrit Rp. 4.500";
             break;
         case '4':
-            cout << "\npilihan No 4. Bubur Ayam LAN Rp. 4.000";
+            cout << "pilihan No 4. Bubur Ayam LAN Rp. 4.000";
             break;
         default:
-            cout << "\nAnda harus memesan !!";
+            cout << "Anda harus memesan !!";
     }
 
     return 0;
